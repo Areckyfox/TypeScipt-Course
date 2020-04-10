ed4907a1f2b23ced2636e061b80ec8b1066d7850
Arkadiusz Lis <alis@efigence.com>
2020-04-10 15:08:42 +0200

    add first custom typescript enum Role {}

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("asdlfkjalfkhlakfhalkjfh");
+// const person:{
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: "Arkadiusz",
+//     age: 38,
+//     hobbies: ['Sport', 'Cooking'],
+//     role: [2, 'author']
+// }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var person = {
+    name: "Arkadiusz",
+    age: 38,
+    hobbies: ["Sport", "Cooking"],
+    role: Role.ADMIN
+};
+console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
