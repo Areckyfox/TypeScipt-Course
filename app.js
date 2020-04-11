f85de007c430054c924238eaa68ccdf4984f8045
Arkadiusz Lis <alis@efigence.com>
2020-04-11 16:55:03 +0200

    union-aliases plus type

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedAges = combine(30, 36);
+var combinedAges = combine(30, 36, 'as-number');
 console.log(combinedAges);
-var combinedNames = combine('Arek ', 'Marysia');
+var combinedStringAges = combine('22', '36', 'as-number');
+console.log(combinedStringAges);
+var combinedNames = combine('Arek ', 'Marysia', 'as-text');
 console.log(combinedNames);
