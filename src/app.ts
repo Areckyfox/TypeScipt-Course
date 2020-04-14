219b372bf233c70f2d73ef56ad14fb6c099bbdc2
Arkadiusz Lis <alis@efigence.com>
2020-04-14 18:32:37 +0200

    add validation input

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,32 @@
+//validation
+
+interface Validateable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validateableInput: Validateable) {
+  let isValid = true;
+  if(validateableInput.required) {
+    isValid = isValid && validateableInput.value.toString().trim().length !== 0;
+  }
+  if(validateableInput.minLength != null && validateableInput.value === 'string') {
+    isValid = isValid && validateableInput.value.length >= validateableInput.minLength;
+  }
+  if(validateableInput.maxLength != null && validateableInput.value === 'string') {
+    isValid = isValid && validateableInput.value.length <= validateableInput.maxLength;
+  }
+  if(validateableInput.min != null && typeof validateableInput.value === 'number') {
+    isValid = isValid && validateableInput.min >= validateableInput.value;
+  } 
+  if(validateableInput.max != null && typeof validateableInput.value === 'number') {
+    isValid = isValid && validateableInput.max <= validateableInput.value;
+  } 
+}
 // autobind decorator
 
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
@@ -35,21 +64,66 @@ class ProjectInput {
 
     this.tileInputElement = this.element.querySelector(
       "#title"
-    )! as HTMLInputElement;
+    ) as HTMLInputElement;
     this.descriptionInputElement = this.element.querySelector(
       "#description"
-    )! as HTMLInputElement;
+    ) as HTMLInputElement;
     this.peopleInputElement = this.element.querySelector(
       "#people"
-    )! as HTMLInputElement;
+    ) as HTMLInputElement;
 
     this.configure();
     this.attach();
   }
+
+  private gatherUserInput():[string, string, number] | void {
+    const enteredTitle = this.tileInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+    
+    const titleValidatable: Validateable = {
+      value: enteredTitle,
+      required: true,
+      minLength: 5
+    }
+    const descriptionValidatable: Validateable = {
+      value: enteredDescription,
+      required: true
+    }
+    const peopleValidatable: Validateable = {
+      value: enteredPeople,
+      required: true,
+      min: 1,
+      max: 5
+    }
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert("invalid input, please try again!");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  private clearInputs() {
+    this.tileInputElement.value ='';
+    this.descriptionInputElement.value ='';
+    this.peopleInputElement.value ='';
+  }
+
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.tileInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs;
+      
+    }
   }
 
   private configure() {
